ae34dc6f58cc0d6351770be4875c88fb8a09b3df
TcHaOcHeK <160489385+TcHaOcHeK@users.noreply.github.com>
2024-05-08 14:24:48 +0600

    Complete this task V2!!

--- app.py
+++ app.py
@@ -10,17 +10,26 @@ def index():
         a = float(request.form['a'])
         b = float(request.form['b'])
         c = float(request.form['c'])
-
-        D = b ** 2 - 4 * a * c
-        if D > 0:
-            x1 = (-b + D ** 0.5) / (2 * a)
-            x2 = (-b - D ** 0.5) / (2 * a)
-            result = f"Корни уравнения: x1 = {x1}, x2 = {x2}"
-        elif D == 0:
-            x = -b / (2 * a)
-            result = f"Уравнение имеет один корень: x = {x}"
-        else:
-            result = "Уравнение не имеет действительных корней"
+        if a !=0 and b!=0 and c!=0:
+            D = b ** 2 - 4 * a * c
+            if D > 0:
+                x1 = (-b + D ** 0.5) / (2 * a)
+                x2 = (-b - D ** 0.5) / (2 * a)
+                result = f"Корни уравнения: x1 = {x1}, x2 = {x2}"
+            elif D == 0:
+                x = -b / (2 * a)
+                result = f"Уравнение имеет один корень: x = {x}"
+            else:
+                result = "Уравнение не имеет действительных корней"
+        elif a == 0:
+            if b == 0:
+                if c == 0:
+                    result = "Бесконечное количество решений"
+                else:
+                    result = "Не имеет решений"
+            else:
+                x = -c / b
+                result = x
 
     return render_template('index.html', result=result)
 
